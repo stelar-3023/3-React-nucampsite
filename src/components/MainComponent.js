6271a1e9ed04ec818c96a8a50a8849e94415273f
Steve Larsen <slarsen-3@att.net>
2020-06-29 15:35:37 -0500

    Controlled Forms

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
-import Home from "./HomeComponent";
-import Directory from "./DirectoryComponent";
-import CampsiteInfo from "./CampsiteInfoComponent";
-import Contact from "./ContactComponent";
+import Directory from ".//DirectoryComponent";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
+import Home from "./HomeComponent";
+import Contact from "./ContactComponent";
+import About from "./AboutComponent";
+import { Switch, Route, Redirect } from "react-router-dom";
+import CampsiteInfo from "./CampsiteInfoComponent";
 import { CAMPSITES } from "../shared/campsites";
 import { COMMENTS } from "../shared/comments";
 import { PARTNERS } from "../shared/partners";
@@ -32,7 +33,24 @@ class Main extends Component {
           promotion={
             this.state.promotions.filter((promotion) => promotion.featured)[0]
           }
-          partner={this.state.partners.filter((partner) => partner.featured)[0]}
+          partner={
+            this.state.partners.filter((promotion) => promotion.featured)[0]
+          }
+        />
+      );
+    };
+
+    const CampsiteWithId = ({ match }) => {
+      return (
+        <CampsiteInfo
+          campsite={
+            this.state.campsites.filter(
+              (campsite) => campsite.id === +match.params.campsiteId
+            )[0]
+          }
+          comments={this.state.comments.filter(
+            (comment) => comment.campsiteId === +match.params.campsiteId
+          )}
         />
       );
     };
@@ -46,7 +64,13 @@ class Main extends Component {
             path="/directory"
             render={() => <Directory campsites={this.state.campsites} />}
           />
+          <Route path="/directory/:campsiteId" component={CampsiteWithId} />
           <Route exact path="/contactus" component={Contact} />
+          <Route
+            exact
+            path="/aboutus"
+            render={() => <About partners={this.state.partners} />}
+          />
           <Redirect to="/home" />
         </Switch>
         <Footer />
